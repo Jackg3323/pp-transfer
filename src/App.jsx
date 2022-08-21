66bcb550b66a105f299f71fc0c14f7c3b4943965
Jack Guthrie <jackg3323@gmail.com>
2022-08-21 14:38:41 +0100

    Add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
+import { useState } from "react";
 import "./App.css";
-import logo from "./logo.svg";
 
 function App() {
+  const [balance, setBalance] = useState(100);
+  const handleTransfer = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const amount = form.transfer.value;
+    if (amount <= balance) {
+      setBalance(balance - amount);
+      <h2>balance is now ${balance}</h2>;
+      console.log(`balance is now ${balance}`);
+    } else {
+      <h2>Balance is too low for transaction</h2>;
+      console.log("Balance is too low for transaction");
+    }
+  };
   return (
-    <div className="text-center">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://beta.reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React (Beta Docs)
-        </a>
-      </header>
-    </div>
+    <form
+      onSubmit={handleTransfer}
+      className="mx-auto w-80 flex-col items-center"
+    >
+      <div>
+        <label htmlFor="transfer" className="sr-only">
+          Transfer
+        </label>
+        <input
+          type="text"
+          placeholder="Transfer"
+          id="transfer"
+          className="rounded border pl-2"
+          name="transfer"
+        />
+      </div>
+      <button type="submit" className="mt-4 bg-purple-500 px-4 py-2 text-white">
+        Transfer
+      </button>
+    </form>
   );
 }
 
