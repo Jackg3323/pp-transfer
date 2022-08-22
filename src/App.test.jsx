8de87265843e84beb3c2cadba29d16ab41d4c90d
Jack Guthrie <jackg3323@gmail.com>
2022-08-22 15:54:44 +0100

    Test update

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,25 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import renderer from "react-test-renderer";
 import App from "./App";
 
-test("renders learn react link", () => {
+it("renders correctly", () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("attempts to transfer an amount from the balance", () => {
+  const transferAmount = 10;
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const input = screen.getByLabelText(/transfer/i);
+  const transferBtn = screen.getByRole("button", { name: "transfer" });
+
+  userEvent.type(input, transferAmount);
+  userEvent.click(transferBtn);
+
+  const h2 = screen.getByRole("heading", { level: 2 });
+
+  expect(h2.textContent).toContain(100 - transferAmount);
 });
