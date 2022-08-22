5ecabe3b5cb34c071a21057dc7fe9730a8a45c56
Jack Guthrie <jackg3323@gmail.com>
2022-08-22 14:31:49 +0100

    Refactor for component (non-working)

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,25 @@
 import { useState } from "react";
 import "./App.css";
+import Form from "./components/Form/Form";
 
 function App() {
   const [balance, setBalance] = useState(100);
   const [outputMsg, setMsg] = useState("");
-  const handleTransfer = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const amount = form.transfer.value;
-    if (balance >= amount) {
-      setBalance(balance - amount);
-      setMsg(`balance is now ${balance}`);
-    } else {
-      setMsg("Balance is too low for transaction");
-    }
+
+  const handleFocus = (event) => {
+    console.log("focused");
   };
+  const handleBlur = (event) => {
+    console.log("Blurred");
+  };
+
   return (
-    <>
-      <form
-        onSubmit={handleTransfer}
-        className="mx-auto w-80 flex-col items-center"
-      >
-        <div>
-          <label htmlFor="transfer" className="sr-only">
-            Transfer
-          </label>
-          <input
-            type="text"
-            placeholder="Transfer"
-            id="transfer"
-            className="rounded border pl-2"
-            name="transfer"
-          />
-        </div>
-        <button
-          type="submit"
-          className="mt-4 bg-purple-500 px-4 py-2 text-white"
-        >
-          Transfer
-        </button>
-      </form>
-      <h2> {outputMsg}</h2>
-    </>
+    <Form
+      balance={balance}
+      setBalance={setBalance}
+      setMsg={setMsg}
+      outputMsg={outputMsg}
+    />
   );
 }
 
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,52 @@
+import PropTypes from "prop-types";
+
+export default function Form({ balance, setBalance, setMsg, outputMsg }) {
+  const handleTransfer = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const amount = form.transfer.value;
+    if (balance >= amount && amount > 0) {
+      setBalance(balance - amount);
+      setMsg(`balance is now ${balance}`);
+    } else {
+      setMsg("Balance is too low for transaction");
+    }
+  };
+
+  return (
+    <>
+      <form
+        onSubmit={handleTransfer}
+        className="mx-auto w-80 flex-col items-center"
+      >
+        <div>
+          <label htmlFor="transfer" className="sr-only">
+            Transfer
+          </label>
+          <input
+            type="text"
+            placeholder="Transfer"
+            id="transfer"
+            className="rounded border pl-2"
+            name="transfer"
+          />
+        </div>
+        <button
+          type="submit"
+          name="transfer"
+          className="mt-4 bg-purple-500 px-4 py-2 text-white"
+        >
+          Transfer
+        </button>
+      </form>
+      <h2> {outputMsg}</h2>
+    </>
+  );
+}
+
+Form.propTypes = {
+  balance: PropTypes.number.isRequired,
+  outputMsg: PropTypes.string.isRequired,
+  setBalance: PropTypes.func.isRequired,
+  setMsg: PropTypes.func.isRequired,
+};
